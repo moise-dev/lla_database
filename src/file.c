1b88c868a104437787d65af92500e3821d77d196
moise-dev <moise.medici@gmail.com>
2026-01-09 13:50:37 +0000

    first commit

---
+++ src/file.c
@@ -0,0 +1,36 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+#include "file.h"
+#include "common.h"
+
+#define PERMS_644  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
+
+int create_db_file(char *filename) {
+    int fd = -1;
+    if ((fd = open(filename, O_RDONLY)) != -1) {
+        printf("File already exists\n");
+        close(fd);
+        return STATUS_ERROR;
+    }
+
+    fd = open(filename, O_RDWR | O_CREAT, PERMS_644);
+    if (fd == -1) {
+        perror("open");
+        return STATUS_ERROR;
+    }
+    return fd;
+}
+
+int open_db_file(char *filename) {
+    int fd = open(filename, O_RDONLY, PERMS_644);
+    if (fd == -1) {
+        perror("open");
+        return STATUS_ERROR;
+    }
+         
+    return fd;
+}
---
+++ src/main.c
@@ -0,0 +1,66 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "common.h"
+#include "file.h"
+
+void print_usage(char *argv[]) {
+  printf("Usage:\n\t%s\n", argv[0]);
+  printf("\t-n: (optional) create new db file\n");
+  printf("\t-f: (mandatory) path to the db file\n");
+}
+
+int main(int argc, char *argv[]) {
+  int c;
+  bool new_file = false;
+  char *filepath = NULL;
+
+  if (argc < 2) {
+    print_usage(argv);
+    return -1;
+  }
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) {
+    switch (c) {
+    case 'n':
+      new_file = true;
+      break;
+    case 'f':
+      filepath = optarg;
+      break;
+    case '?':
+      printf("Unknown option -%c\n", c);
+    default:
+      print_usage(argv);
+      return -1;
+    }
+  }
+
+  if (filepath == NULL) {
+    printf("-f argument is mandatory.\n");
+    return -1;
+  }
+
+  if (new_file == true) {
+    int fd = create_db_file(filepath);
+    if (fd == STATUS_ERROR) {
+      printf("Unable to create db file.\n");
+      return STATUS_ERROR;
+    }
+    printf("Created new file with fd %d.\n", fd);
+    close(fd);
+  } else {
+    int fd = open_db_file(filepath);
+    if (fd == STATUS_ERROR) {
+      printf("Unable to open db file.\n");
+      return STATUS_ERROR;
+    }
+    printf("Opened new file with fd %d.\n", fd);
+    close(fd);
+  }
+
+  printf("New file: %d\n", new_file);
+  printf("Filename: %s\n", filepath);
+  return 0;
+}
