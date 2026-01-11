46bbb6936954dd587dc5f8fb3acd2cd6b32a1428
moise-dev <moise.medici@gmail.com>
2026-01-11 11:59:58 +0000

    change list visual

--- src/parse.c
+++ src/parse.c
@@ -12,7 +12,7 @@ void list_employees(
     struct employee_t* employees
 ) {
     for (int i = 0; i < headerOut->count; i++) {
-        printf("Employee %d\n", i);
+        printf("Employee %d\n", i + 1);
         printf("\tName: %s\n", employees[i].name);
         printf("\tAddress: %s\n", employees[i].address);
         printf("\tHours: %d\n", employees[i].hours);
